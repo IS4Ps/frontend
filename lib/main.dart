4a3c236f52d93b2b4b34428bec6e363932a3f813
heewon411 <heewon2842@naver.com>
2026-04-29 14:27:18 +0900

    [FEAT/#41] 안드로이드에서 3D 캐릭터 화면에 띄우기

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+
 import 'view_model/quest/quest_view_model.dart';
 import 'start_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 웹뷰 플랫폼 초기화 로직 추가
+  if (WebViewPlatform.instance == null) {
+    WebViewPlatform.instance = AndroidWebViewPlatform();
+  }
+
   await dotenv.load(fileName: ".env");
 
   runApp(
